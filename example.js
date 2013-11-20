478687831d30f90652a1c4f02c910e4b83eccd8d
deoxxa <deoxxa@fknsrs.biz>
2013-11-20 22:31:10 +1100

    load a file as specified on the command line

--- example.js
+++ example.js
@@ -4,7 +4,7 @@ var xmldom = require("xmldom");
 
 var c14n = require("./")();
 
-var xmlData = require("fs").readFileSync("./example.xml", "utf8"),
+var xmlData = require("fs").readFileSync(process.argv[2], "utf8"),
     document = (new xmldom.DOMParser()).parseFromString(xmlData);
 
 var canonicaliser = c14n.createCanonicaliser("http://www.w3.org/2001/10/xml-exc-c14n#WithComments");
